61986e69b6e7723bdb07f67c2ba756ef8c2f2f3e
Rick <49814330+KARASAWA-RICK@users.noreply.github.com>
2024-05-15 14:00:56 +0800

    test

--- assets/Scripts/Base/Component/Touch/Swipe.ts
+++
@@ -1,7 +0,0 @@
-import { _decorator, Component, Node } from "cc";
-const { ccclass, property } = _decorator;
-
-@ccclass("Swipe")
-export class Swipe extends Component {
-  hasSwiped: boolean = false;
-}
